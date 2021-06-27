df03e8b3a5fc6c0ab44b7748cd8b062998bdf533
Matthew Dharm <85530043+mdharm@users.noreply.github.com>
2021-06-26 17:37:47 -0700

    First pass at MattSimulator; no gravity
    This implementation of MattSimulator has no gravity effect; masses only
    travel at constant (initial) velocity.

--- src/matt.rs
+++ src/matt.rs
@@ -1,149 +1,15 @@
 use super::*;
-use Tree::*;
-
-#[derive(Debug)]
-struct TreeNode {
-    center: Point,
-    mass: Float,
-    left: Tree,
-    right: Tree,
-}
-
-#[derive(Debug)]
-enum Tree {
-    Node(Box<TreeNode>),
-    Leaf(Mass),
-}
-
-impl Tree {
-    /*
-    fn size(&self) -> i32 {
-        match self {
-            Leaf(_) => 1,
-            Node(n) => n.left.size() + n.right.size(),
-        }
-    }
-    */
-
-    fn new_node(left: Tree, right: Tree) -> Tree {
-        let new_mass = left.mass() + right.mass();
-        Node(Box::new(TreeNode {
-            center: left
-                .center()
-                .scale(left.mass())
-                .add(right.center().scale(right.mass()))
-                .scale(1.0 / new_mass),
-            mass: new_mass,
-            left,
-            right,
-        }))
-    }
-
-    fn center(&self) -> Point {
-        match self {
-            Leaf(m) => m.position,
-            Node(n) => n.center,
-        }
-    }
-
-    fn mass(&self) -> Float {
-        match self {
-            Leaf(m) => m.mass,
-            Node(n) => n.mass,
-        }
-    }
-
-    fn add_mass(self, mass_ref: Mass) -> Self {
-        match self {
-            Leaf(lm) => Tree::new_node(Leaf(lm), Leaf(mass_ref)),
-            Node(node) => {
-                let center = mass_ref.position;
-                // ignore the effect of node.mass, because it would be same for left and right
-                let mut left_force =
-                    (node.left.mass()) / node.left.center().minus(center).magnitude_squared();
-                let mut right_force =
-                    (node.right.mass()) / node.right.center().minus(center).magnitude_squared();
-                if left_force.is_nan() {
-                    println!("left NaN");
-                    left_force = rand::thread_rng().gen::<Float>();
-                }
-                if right_force.is_nan() {
-                    println!("right NaN");
-                    right_force = rand::thread_rng().gen::<Float>();
-                }
-                if right_force > left_force {
-                    Tree::new_node(node.left, node.right.add_mass(mass_ref))
-                } else {
-                    Tree::new_node(node.left.add_mass(mass_ref), node.right)
-                }
-            }
-        }
-    }
-
-    fn update_with(&mut self, force: Point) {
-        match self {
-            Node(i) => {
-                let diff = i.left.center().minus(i.right.center());
-                let f = diff
-                    .unit_vector()
-                    .scale((i.left.mass() * i.right.mass()) / diff.magnitude_squared().sqrt());
-                i.left.update_with(force.add(f));
-                i.right.update_with(force.add(f.inverse()));
-            }
-            Leaf(mass) => {
-                let mut point_mass = mass;
-                point_mass.velocity = point_mass.velocity.add(force.scale(1.0 / point_mass.mass));
-                point_mass.position = point_mass.position.add(point_mass.velocity);
-            }
-        }
-    }
-}
-
-struct TreeIter<'a> {
-    stack: Vec<&'a Tree>,
-}
-
-impl<'a> TreeIter<'a> {
-    fn new(tree: &'a Tree) -> TreeIter<'a> {
-        let mut i = TreeIter { stack: Vec::new() };
-        i.load(tree);
-        i
-    }
-    fn load(&mut self, tree: &'a Tree) {
-        self.stack.push(tree);
-        match tree {
-            Leaf(_) => {}
-            Node(n) => {
-                self.load(&n.left);
-            }
-        }
-    }
-}
-
-impl<'a> Iterator for TreeIter<'a> {
-    type Item = &'a Mass;
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.stack.pop() {
-            Some(Node(n)) => {
-                self.load(&n.right);
-                self.next()
-            }
-            Some(Leaf(m)) => Some(m),
-            None => None,
-        }
-    }
-}
 
 #[derive(Debug)]
 pub struct MattFactory;
 
 impl SimFactory for MattFactory {
     fn new(&self, count: usize) -> Box<dyn Simulator> {
-        let mut tree = Leaf(Mass::new_random());
+        let mut masses: Vec<Mass> = Vec::new();
         for _i in 1..count {
-            tree = tree.add_mass(Mass::new_random());
+            masses.push(Mass::new_random());
         }
-        Box::new(MattSimulator { tree })
+        Box::new(MattSimulator { masses })
     }
 
     fn name(&self) -> String {
@@ -153,28 +19,21 @@ impl SimFactory for MattFactory {
 
 #[derive(Debug)]
 struct MattSimulator {
-    tree: Tree,
+    masses: Vec<Mass>,
 }
 
-impl MattSimulator {
-    fn new_tree(&self) -> Tree {
-        let mut iter = self.mass_iter();
-        let mut tree = Leaf(*iter.next().unwrap());
-        for mass in iter {
-            tree = tree.add_mass(*mass);
-        }
-        tree.update_with(Point::ZERO);
-        tree
-    }
-}
+impl MattSimulator {}
 
 impl Simulator for MattSimulator {
     fn step(&mut self) {
-        self.tree = self.new_tree();
+        for x in self.masses.iter_mut() {
+            x.position = x.position.add(x.velocity);
+            //x.position += x.velocity;
+        }
     }
 
     fn mass_iter<'a>(&'a self) -> Box<dyn Iterator<Item = &Mass> + 'a> {
-        Box::new(TreeIter::new(&self.tree))
+        Box::new(self.masses.iter())
     }
 }
 
